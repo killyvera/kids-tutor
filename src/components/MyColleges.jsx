adaa23aa69c99d0a9b81acdb19a2cef3f9499a57
killyvera <david.esmeralda@gmail.com>
2023-03-31 01:19:01 -0600

    added features of product

--- src/components/MyColleges.jsx
+++ src/components/MyColleges.jsx
@@ -31,7 +31,7 @@ const MyColleges = () => {
             {
                 breakpoint: 1024,
                 settings: {
-                    arrows: true,
+                    arrows: false,
                     slidesToShow: 3,
                     slidesToScroll: 1,
                     infinite: true,
@@ -41,7 +41,7 @@ const MyColleges = () => {
             {
                 breakpoint: 600,
                 settings: {
-                    arrows: true,
+                    arrows: false,
                     slidesToShow: 2,
                     slidesToScroll: 1,
                     initialSlide: 0,
