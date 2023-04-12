96dfac63327bdb7c88a8e990c6e56ded902d32c5
killyvera <david.esmeralda@gmail.com>
2023-04-12 10:30:26 -0600

    added some

--- src/components/CollectionSSR.jsx
+++ src/components/CollectionSSR.jsx
@@ -1,10 +1,10 @@
 import {serializeModel} from '@aws-amplify/datastore/ssr'
 import { DataStore, Amplify, withSSRContext } from 'aws-amplify'
-import { Colleges } from '@/models';
-import 
+import { Colleges } from '@/models'; 
 
 export default async function getStaticPhats() {
   return (
+    <p>hi</p>
 
   )
 }
