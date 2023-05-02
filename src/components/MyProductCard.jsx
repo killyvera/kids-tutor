6207ae87c89c86b41a110ded09159e7fde5247ec
killyvera <david.esmeralda@gmail.com>
2023-05-02 11:24:45 -0600

    added product card & list product component and ssr to product index and id file was added

---
+++ src/components/MyProductCard.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import MyBadgeList from "./MyBadgeList";
+
+const MyProductCard = ({ element, type }) => {
+  console.log(element, type);
+  return (
+    <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4 bg-white ">
+      <Image
+        alt="Blog image description from backend"
+        height={"1"}
+        width={"400"}
+        src={element?.cover}
+        className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-"
+      />
+      <div className="flex justify-between w-full mb-2">
+        <p className="text-sm text-gray-500">{element?.sku}</p>
+        <p className="text-sm text-gray-500">{element?.stock}</p>
+      </div>
+      <MyBadgeList resourceId={element?.id} />
+      <Link
+        href={"/" + type + "/" + element?.id}
+        className="text-lg font-bold sm:text-xl md:text-2xl"
+      >
+        {element?.name}
+      </Link>
+      <p className="text-sm text-black">
+        {element?.description.slice(0, 150) + "..."}
+      </p>
+    </div>
+  );
+};
+export default MyProductCard;
