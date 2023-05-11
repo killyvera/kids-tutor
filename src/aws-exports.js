e92118556f98722e5d143c5122aa0e03fe2eb8bf
killyvera <david.esmeralda@gmail.com>
2023-05-11 15:35:00 -0600

    added auth face fix

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -20,8 +20,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "https://main.d1c8o6kxtv8w6n.amplifyapp.com/",
+        "redirectSignOut": "https://main.d1c8o6kxtv8w6n.amplifyapp.com/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
