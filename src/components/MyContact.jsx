939f7594e276e0e1e91caeb477ad2c5d1574d2fb
killyvera <david.esmeralda@gmail.com>
2023-05-18 04:26:31 -0600

    added terms and conditons, privacy-policy and contact

---
+++ src/components/MyContact.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { Contact } from "../models"; // Importa el modelo Contact de DataStore
+import { DataStore } from "aws-amplify";
+
+export default function MyContact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Crea una nueva instancia de Contact con los datos del formulario
+      const newContact = await DataStore.save(
+        new Contact({
+          name: name,
+          email: email,
+          message: message,
+        })
+      );
+
+      console.log("New contact:", newContact);
+
+      // Resetea los campos del formulario
+      setName("");
+      setEmail("");
+      setMessage("");
+    } catch (error) {
+      console.error("Error creating contact:", error);
+    }
+  };
+
+  return (
+    <div className="text-gray-900 py-10 pr-0 pl-0 bg-white m-5 rounded">
+      <section className="text-gray-800">
+        <div className="flex flex-wrap">
+          <div className="grow-0 shrink-0 basis-auto mb-6 md:mb-0 w-full md:w-6/12 px-3 lg:px-6">
+            <h2 className="text-3xl font-bold mb-6">Contact us</h2>
+            <p className="text-gray-500 mb-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laudantium, modi accusantium ipsum corporis quia asperiores
+              dolorem nisi corrupti eveniet dolores ad maiores repellendus enim
+              autem omnis fugiat perspiciatis? Ad, veritatis.
+            </p>
+            <p className="text-gray-500 mb-2">New York, 94126, United States</p>
+            <p className="text-gray-500 mb-2">+ 01 234 567 89</p>
+            <p className="text-gray-500 mb-2">info@gmail.com</p>
+          </div>
+          <div className="grow-0 shrink-0 basis-auto mb-12 md:mb-0 w-full md:w-6/12 px-3 lg:px-6">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group mb-6">
+                <input
+                  type="text"
+                  className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleInput7"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group mb-6">
+                <input
+                  type="email"
+                  className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleInput8"
+                  placeholder="Email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group mb-6">
+                <textarea
+                  className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleFormControlTextarea13"
+                  rows="3"
+                  placeholder="Message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  required
+                ></textarea>
+              </div>
+              
+              <button
+                type="submit"
+                className="w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                Send
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,112 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="bg-white py-8 px-4 md:px-8">
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-2xl font-bold mb-4">
+          Política de Privacidad de KidsTutor.co
+        </h1>
+        <p className="mb-4">Fecha de última actualización: [Fecha]</p>
+
+        <div className="bg-white rounded-lg shadow-md p-6 mb-4">
+          <h2 className="text-xl font-bold mb-2">1. Información recopilada</h2>
+          <p className="mb-4">
+            Recopilamos cierta información personal cuando te registras y
+            utilizas KidsTutor.co. Esto puede incluir tu nombre, dirección de
+            correo electrónico, información de inicio de sesión, edad y
+            cualquier otra información que elijas proporcionar voluntariamente.
+            También recopilamos información técnica, como tu dirección IP, tipo
+            de navegador, proveedor de servicios de Internet y registros de
+            interacción con nuestro sitio web.
+          </p>
+        </div>
+
+        <div className="bg-white rounded-lg shadow-md p-6 mb-4">
+          <h2 className="text-xl font-bold mb-2">2. Uso de la información</h2>
+          <p className="mb-4">
+            Utilizamos la información recopilada para proporcionarte nuestros
+            servicios, mejorar la experiencia del usuario, personalizar el
+            contenido y los anuncios, enviar comunicaciones relacionadas con el
+            servicio, responder a consultas y brindar asistencia técnica.
+            También podemos utilizar información agregada y anónima para
+            realizar análisis y mejorar nuestros servicios.
+          </p>
+        </div>
+
+        <div className="bg-white rounded-lg shadow-md p-6 mb-4">
+          <h2 className="text-xl font-bold mb-2">2. Uso de la información</h2>
+          <p className="mb-4">
+            Utilizamos la información recopilada para proporcionarte nuestros
+            servicios, mejorar la experiencia del usuario, personalizar el
+            contenido y los anuncios, enviar comunicaciones relacionadas con el
+            servicio, responder a consultas y brindar asistencia técnica.
+            También podemos utilizar información agregada y anónima para
+            realizar análisis y mejorar nuestros servicios.
+          </p>
+        </div>
+
+        <div className="bg-white rounded-lg shadow-md p-6 mb-4">
+          <h2 className="text-xl font-bold mb-2">3. Compartir información</h2>
+          <p className="mb-4">
+            No compartiremos tu información personal con terceros, excepto en
+            los siguientes casos: - Con tu consentimiento expreso. - Con
+            proveedores de servicios y socios comerciales que trabajen en
+            nuestro nombre y estén sujetos a acuerdos de confidencialidad. -
+            Para cumplir con las leyes aplicables, regulaciones o solicitudes
+            legales, o para proteger nuestros derechos legales, propiedad o
+            seguridad, así como los derechos, propiedad o seguridad de nuestros
+            usuarios u otros. - En caso de una fusión, adquisición o venta de la
+            totalidad o una parte sustancial de nuestros activos, donde la
+            información del usuario sea uno de los activos transferidos.
+          </p>
+        </div>
+
+        <div className="bg-white rounded-lg shadow-md p-6 mb-4">
+          <h2 className="text-xl font-bold mb-2">
+            4. Seguridad de la información
+          </h2>
+          <p className="mb-4">
+            Implementamos medidas de seguridad apropiadas para proteger la
+            información personal de nuestros usuarios. Sin embargo, no podemos
+            garantizar la seguridad absoluta de la información transmitida a
+            través de Internet. Los usuarios son responsables de mantener la
+            confidencialidad de sus credenciales de inicio de sesión y
+            notificarnos de cualquier uso no autorizado de su cuenta.
+          </p>
+        </div>
+
+        <div className="bg-white rounded-lg shadow-md p-6 mb-4">
+          <h2 className="text-xl font-bold mb-2">
+            5. Cookies y tecnologías similares
+          </h2>
+          <p className="mb-4">
+            Utilizamos cookies y tecnologías similares para recopilar
+            información y mejorar la experiencia del usuario. Puedes ajustar la
+            configuración de tu navegador para rechazar cookies, pero esto puede
+            afectar la funcionalidad de KidsTutor.co.
+          </p>
+        </div>
+        <div className="bg-white rounded-lg shadow-md p-6 mb-4">
+          <h2 className="text-xl font-bold mb-2">
+            6. Enlaces a sitios de terceros
+          </h2>
+          <p className="mb-4">
+            KidsTutor.co puede contener enlaces a sitios web de terceros. No nos
+            hacemos responsables de las prácticas de privacidad o el contenido
+            de dichos sitios. Te recomendamos leer las políticas de privacidad
+            de esos sitios antes de proporcionar cualquier información personal.
+          </p>
+        </div>
+
+        <p className="mt-4">
+          Si tienes alguna pregunta o inquietud sobre nuestra política de
+          privacidad, no dudes en contactarnos a través de [dirección de correo
+          electrónico o formulario de contacto].
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
