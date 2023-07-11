71e5b56bbaf5f5aa81839054fa50b94c6307615b
killyvera <david.esmeralda@gmail.com>
2023-07-11 15:55:33 -0600

    added second test

--- src/pages/videos/test.jsx
+++ src/pages/videos/test.jsx
@@ -6,7 +6,7 @@ const TestPage = () => {
 
   useEffect(() => {
     // Obtén el enlace actualizado del video desde la API de AppSync
-    // Reemplaza con tu enlace actualizado
+    const videoLink = `https://www.youtube.com/watch?v=leIK1aUOF-Q&ab`; // Reemplaza con tu enlace actualizado
 
     // Redirige al usuario al enlace del video en YouTube
     const redirectToYouTube = () => {
@@ -15,17 +15,23 @@ const TestPage = () => {
           /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
             navigator.userAgent
           );
-        const videoLink = `leIK1aUOF-Q&ab`; // Reemplaza con tu propio enlace del video
+        const youtubeAppLink = "leIK1aUOF-Q&ab"; // Reemplaza con tu propio enlace del video
 
-        if (isMobile) {
-          window.location.href = `youtube://watch?v=${videoLink}`;
-        } else {
-          window.location.href = `https://www.youtube.com/watch?v=${videoLink}`;
-        }
+        const handleRedirect = () => {
+          const confirmOpenApp = window.confirm("¿Deseas abrir la aplicación de YouTube?");
+
+          if (confirmOpenApp && isMobile) {
+            window.location.href = `youtube://watch?v=${youtubeAppLink}`;
+          } else {
+            window.location.href = `https://www.youtube.com/watch?v=${videoLink}`;
+          }
+        };
+
+        handleRedirect();
       }
     };
 
-    router.push("/");
+    // router.push("/");
 
     // Llama a la función redirectToYouTube cuando sea necesario, por ejemplo, cuando se escanea el código QR
     redirectToYouTube();
